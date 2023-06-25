e9686292846580f60c4104bd63edeb5dc6dedc57
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-06-25 21:05:07 +0300

    add HW_4

---
+++ HW_4/Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        BinTree tree = new BinTree<>();
+        Random random = new Random();
+
+
+        for (Integer i = 0; i < 10; i++) {
+            tree.add(random.nextInt(100));
+        }
+    }
+}
